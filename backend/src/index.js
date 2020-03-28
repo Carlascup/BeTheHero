ce987815850127c6bc5197db8aff7ba452d176cc
Carla Amaral <carlafa08@gmail.com>
2020-03-28 13:58:32 -0300

    Iniciando o projeto

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+  * Route Params: Parâmetros utilizados para identificar rescursos /:id
+  * Rquest body: Corpo da requisição, utilizado para criar ou alterar recursos
+  * 
+  */
+
+  /**
+   * SQL: MySQl, SQLite - Vamos utilizar esse
+   * NoSQL: MongoDB......
+   * 
+   */
+
+
+app.listen(3333);
